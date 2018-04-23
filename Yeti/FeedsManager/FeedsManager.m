2f8d73e661f965bb95ef7098a78255617409df02
Nikhil Nigade <nikhil@dezinezync.com>
2018-04-23 11:53:18 +0530

    use prod server

--- Yeti/FeedsManager/FeedsManager.m
+++ Yeti/FeedsManager/FeedsManager.m
@@ -989,7 +989,7 @@ FMNotification _Nonnull const SubscribedToFeed = @"com.yeti.note.subscribedToFee
         [session setValue:sessionSession forKeyPath:@"session"];
         
         session.baseURL = [NSURL URLWithString:@"http://192.168.1.15:3000"];
-//        session.baseURL = [NSURL URLWithString:@"https://yeti.dezinezync.com"];
+        session.baseURL = [NSURL URLWithString:@"https://yeti.dezinezync.com"];
 #ifndef DEBUG
         session.baseURL = [NSURL URLWithString:@"https://yeti.dezinezync.com"];
 #endif
