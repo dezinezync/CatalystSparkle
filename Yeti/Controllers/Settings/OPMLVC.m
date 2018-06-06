1c430c40635d8f68e3c685ec1b9a564ffc2ef892
Nikhil Nigade <nikhil@dezinezync.com>
2018-06-06 17:41:16 +0530

    production urls

--- Yeti/Controllers/Settings/OPMLVC.m
+++ Yeti/Controllers/Settings/OPMLVC.m
@@ -291,6 +291,7 @@
     self.ioDoneButton.enabled = NO;
     
     NSString *url = formattedString(@"http://192.168.1.15:3000/user/opml");
+    url = @"https://api.elytra.app/user/opml";
 #ifndef DEBUG
     url = @"https://api.elytra.app/user/opml";
 #endif
