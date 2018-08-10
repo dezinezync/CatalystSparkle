e2c17dbe8fe923978decd253e3d393046bd0bc6d
Nikhil Nigade <nikhil@dezinezync.com>
2018-08-10 18:19:56 +0530

    added black icon

--- Yeti/Controllers/Settings/MiscVC.m
+++ Yeti/Controllers/Settings/MiscVC.m
@@ -32,7 +32,7 @@ NSString *const kMiscSettingsCell = @"settingsCell";
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 2;
+    return 3;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
@@ -48,7 +48,7 @@ NSString *const kMiscSettingsCell = @"settingsCell";
     cell.textLabel.textColor = theme.titleColor;
     
     NSString *selectedIcon = UIApplication.sharedApplication.alternateIconName;
-    NSInteger selected = selectedIcon == nil ? 0 : 1;
+    NSInteger selected = selectedIcon == nil ? 0 : ([selectedIcon isEqualToString:@"dark"] ? 1 : 2);
     
     // Configure the cell...
     switch (indexPath.row) {
@@ -59,6 +59,7 @@ NSString *const kMiscSettingsCell = @"settingsCell";
             cell.textLabel.text = @"Dark";
             break;
         default:
+            cell.textLabel.text = @"Black";
             break;
     }
     
@@ -80,7 +81,18 @@ NSString *const kMiscSettingsCell = @"settingsCell";
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    NSString *name = indexPath.row == 0 ? nil : @"dark";
+    NSString *name = nil;
+    
+    switch (indexPath.row) {
+        case 1:
+            name = @"dark";
+            break;
+        case 2:
+            name = @"black";
+            break;
+        default:
+            break;
+    }
     
     [[UIApplication sharedApplication] setAlternateIconName:name completionHandler:^(NSError * _Nullable error) {
         
@@ -93,10 +105,16 @@ NSString *const kMiscSettingsCell = @"settingsCell";
                 UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
                 cell.accessoryType = UITableViewCellAccessoryCheckmark;
                 
-                NSIndexPath *otherIndexPath = [NSIndexPath indexPathForRow:(indexPath.row == 0 ? 1 : 0) inSection:0];
-                cell = [tableView cellForRowAtIndexPath:otherIndexPath];
-                
-                cell.accessoryType = UITableViewCellAccessoryNone;
+                for (NSUInteger idx = 0, rows = [self.tableView numberOfRowsInSection:0]; idx < rows; idx++) { @autoreleasepool {
+                    if (idx == indexPath.row) {
+                        continue;
+                    }
+                    
+                    NSIndexPath *otherIndexPath = [NSIndexPath indexPathForRow:idx inSection:0];
+                    cell = [tableView cellForRowAtIndexPath:otherIndexPath];
+                    
+                    cell.accessoryType = UITableViewCellAccessoryNone;
+                } }
                 
             });
         }
