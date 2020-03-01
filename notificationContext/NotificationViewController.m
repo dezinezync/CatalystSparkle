9ffccee56f8e3fcc77ad811316da09dbe71fa750
Nikhil Nigade <nikhil@dezinezync.com>
2020-03-01 16:08:17 +0530

    code cleanup and image caching

--- notificationContext/NotificationViewController.m
+++ notificationContext/NotificationViewController.m
@@ -12,12 +12,16 @@
 
 @interface NotificationViewController () <UNNotificationContentExtension>
 
+@property (weak, nonatomic) IBOutlet UIImageView *coverImage;
+
 @property (weak, nonatomic) IBOutlet UIImageView *favicon;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *subtitleLabel;
 
 @property (weak, nonatomic) IBOutlet UIStackView *mainStack;
 @property (weak, nonatomic) IBOutlet UIStackView *subStack;
+
+@property (nonatomic, strong) NSURLCache *cache;
 
 @end
 
@@ -35,8 +39,6 @@
     self.favicon.layer.cornerRadius = 4.f;
     self.favicon.layer.cornerCurve = kCACornerCurveContinuous;
     self.favicon.layer.masksToBounds = YES;
-    
-    self.view.backgroundColor = UIColor.systemBackgroundColor;
 }
 
 - (void)viewDidLayoutSubviews {
@@ -59,35 +61,152 @@
     
     if (path != nil) {
         
-        CGFloat scale = UIScreen.mainScreen.scale;
-        CGFloat width = 32 * scale;
-        
-        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://images.weserv.nl/?url=%@&w=%@&dpr=%@&q=%@&we", path, @(width), @(scale), @(90)]];
-        
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-            
-            NSData *data = [NSData dataWithContentsOfURL:url];
-            
-            if (data != nil) {
-            
-                dispatch_async(dispatch_get_main_queue(), ^{
-                   
-                    UIImage *image = [UIImage imageWithData:data];
-                    
-                    if (image != nil) {
-                        self.favicon.image = image;
-                    }
-                    
-                });
-                
-            }
-            
-        });
+        [self setupFavicon:path];
         
     }
     
+    path = [notification.request.content.userInfo valueForKey:@"coverImage"];
+    
+//    if (path != nil) {
+//        [self setupCoverImage:path];
+//    }
+//    else {
+        self.coverImage.hidden = YES;
+//    }
+    
     [self.titleLabel sizeToFit];
     [self.subtitleLabel sizeToFit];
+    
+}
+
+- (void)setupFavicon:(NSString *)path {
+    
+    if (!path || [[path stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] length] == 0) {
+        return;
+    }
+    
+    CGFloat scale = UIScreen.mainScreen.scale;
+    CGFloat width = 32 * scale;
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://images.weserv.nl/?url=%@&w=%@&dpr=%@&q=%@&we", path, @(width), @(scale), @(90)]];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:60];
+    
+    __block NSCachedURLResponse *cached = [self.cache cachedResponseForRequest:request];
+    
+    if (cached != nil) {
+        
+        UIImage *image = [UIImage imageWithData:cached.data];
+        
+        self.favicon.image = image;
+        
+        return;
+        
+    }
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+            
+        [[NSURLSession.sharedSession dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            
+            if (error != nil) {
+                NSLog(@"Error loading %@: %@", url, error.localizedDescription);
+                return;
+            }
+            
+            if (data == nil) {
+                return;
+            }
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+               
+                UIImage *image = [UIImage imageWithData:data];
+                
+                if (image == nil) {
+                    return;
+                }
+                
+                cached = [[NSCachedURLResponse alloc] initWithResponse:response data:data];
+                
+                [self.cache storeCachedResponse:cached forRequest:request];
+                
+                self.favicon.image = image;
+                
+            });
+            
+        }] resume];
+        
+    });
+    
+}
+
+- (void)setupCoverImage:(NSString *)path {
+    
+    if (!path || [[path stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] length] == 0) {
+        return;
+    }
+    
+    CGFloat scale = UIScreen.mainScreen.scale;
+    CGFloat width = self.titleLabel.preferredMaxLayoutWidth + 32.f + 8.f;
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://images.weserv.nl/?url=%@&w=%@&dpr=%@&q=%@&we", path, @(width), @(scale), @(90)]];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:60];
+    
+    __block NSCachedURLResponse *cached = [self.cache cachedResponseForRequest:request];
+    
+    if (cached != nil) {
+        
+        UIImage *image = [UIImage imageWithData:cached.data];
+        
+        self.coverImage.image = image;
+
+        [self.coverImage layoutIfNeeded];
+        
+        return;
+    }
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        
+        [[NSURLSession.sharedSession dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            
+            if (error != nil) {
+                NSLog(@"Error loading %@: %@", url, error.localizedDescription);
+                return;
+            }
+            
+            if (data == nil) {
+                return;
+            }
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+               
+                UIImage *image = [UIImage imageWithData:data];
+                
+                if (image == nil) {
+                    return;
+                }
+                
+                cached = [[NSCachedURLResponse alloc] initWithResponse:response data:data];
+                
+                [self.cache storeCachedResponse:cached forRequest:request];
+                
+                self.coverImage.image = image;
+
+                [self.coverImage layoutIfNeeded];
+                
+            });
+            
+        }] resume];
+        
+    });
+    
+}
+
+#pragma mark - Getters
+
+- (NSURLCache *)cache {
+    
+    return NSURLCache.sharedURLCache;
     
 }
 
