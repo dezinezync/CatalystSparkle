a95fab4b0b6dbb3228e6c2950b545ba5718070c6
Nikhil Nigade <nikhil@dezinezync.com>
2020-08-20 12:50:57 +0530

    added SDWebImageSwiftUI

--- UnreadsWidget/UnreadsWidget.swift
+++ UnreadsWidget/UnreadsWidget.swift
@@ -9,6 +9,7 @@
 import WidgetKit
 import SwiftUI
 import Intents
+import SDWebImageSwiftUI
 
 struct Provider: IntentTimelineProvider {
     
@@ -41,6 +42,26 @@ struct Provider: IntentTimelineProvider {
         
         completion(timeline)
    
+    }
+    
+    public func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<SimpleEntries>) -> Void) {
+        
+        var entries: [SimpleEntries] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        
+        let entryCol = SimpleEntries(date: currentDate, entries: [
+            SimpleEntry(date: Date(), title: "iOS 14 Review with a long title to check", author: "Federico Viticci", blog: "MacStories", image: nil, imageURL: nil, configuration: configuration),
+            SimpleEntry(date: Date(), title: "iOS 15 Review", author: "John Gruber", blog: "Daring Fireball", image: nil, imageURL: nil, configuration: configuration)
+        ])
+        
+        entries.append(entryCol)
+        
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        
+        completion(timeline)
+        
     }
     
 }
@@ -108,12 +129,14 @@ struct ArticleView : View {
                 
                 Spacer()
                 
-                Image(systemName: "square.dashed")
+                WebImage(url: URL(string: "https://blog.elytra.app/wp-content/uploads/2020/06/newAppIcon@2x.png"))
+                    .placeholder(Image(systemName: "square.dashed"))
                     .frame(width: 40, height: 40, alignment: .trailing)
                     .foregroundColor(.secondary)
                     .alignmentGuide(HorizontalAlignment.trailing) { _ in
                         0
                     }
+                    .scaledToFit()
                 
             }
             
