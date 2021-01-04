7e7073f37ae2b1729ced65830064ba5ea37c478a
Nikhil Nigade <nikhil@dezinezync.com>
2021-01-04 09:18:26 +0530

    various
    - Fixes directional marking read bug which caused all articles to get marked as read. h/t Gui.
    
    - Added Feeds to the iOS Search Index. You can now directly open feeds by their names (or custom names if you have one set). I'll eventually expand this to your bookmarks and other articles as well.

---
+++ Yeti/Managers/DBManager/DBManager+Spotlight.h
@@ -0,0 +1,17 @@
+//
+//  DBManager+Spotlight.h
+//  Elytra
+//
+//  Created by Nikhil Nigade on 04/01/21.
+//  Copyright © 2021 Dezine Zync Studios. All rights reserved.
+//
+
+#import "DBManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DBManager (Spotlight)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Yeti/Managers/DBManager/DBManager+Spotlight.m
@@ -0,0 +1,13 @@
+//
+//  DBManager+Spotlight.m
+//  Elytra
+//
+//  Created by Nikhil Nigade on 04/01/21.
+//  Copyright © 2021 Dezine Zync Studios. All rights reserved.
+//
+
+#import "DBManager+Spotlight.h"
+
+@implementation DBManager (Spotlight)
+
+@end
