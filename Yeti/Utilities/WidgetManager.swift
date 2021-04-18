27f4ccbd59047313727c9f73c97f18a5eb31f444
Nikhil Nigade <nikhil@dezinezync.com>
2021-04-18 09:28:22 +0530

    working folder selection

--- Yeti/Utilities/WidgetManager.swift
+++ Yeti/Utilities/WidgetManager.swift
@@ -9,20 +9,22 @@
 import Foundation
 import WidgetKit
 import Models
-import Networking
-import DBManager
-import BackgroundTasks
+import Intents
 
 @objcMembers public class WidgetManager: NSObject {
     
     static var usingUnreadsWidget: Bool = false
     static var usingBloccsWidget: Bool = false
     
+    static var usingFoldersWidget: Bool = false
+    static var selectedFolder: WidgetFolder?
+    
     public static func updateState() {
         
         // set all to false.
         usingUnreadsWidget = false
         usingBloccsWidget = false
+        usingFoldersWidget = false
         
         WidgetCenter.shared.getCurrentConfigurations { result in
             
@@ -39,6 +41,22 @@ import BackgroundTasks
                     }
                     else if (config.kind == "Bloccs Widget") {
                         usingBloccsWidget = true
+                    }
+                    else if (config.kind == "Folders Widget") {
+                        usingFoldersWidget = true
+                        
+                        if let foldersIntent: INIntent = config.configuration,
+                           let folder = foldersIntent.value(forKey: "folders") as? INObject,
+                           let identifer = folder.identifier as NSString? {
+                            
+                            let displayString = folder.displayString
+                            
+                            let widgetFolder = WidgetFolder(title: displayString, folderID: UInt(identifer.integerValue))
+                            
+                            self.selectedFolder = widgetFolder
+                            
+                        }
+                        
                     }
                     
                 }
